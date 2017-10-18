b7e21c50654bf5fb7f5b3fdd7ddf3438ae90a3f2
erikiado <erikiado@gmail.com>
2017-10-18 15:24:30 -0500

    separate files

---
+++ constants.js
@@ -0,0 +1,7 @@
+const WORLD_SIDE_SIZE = 7;
+const WINDOW_HEIGHT = window.innerHeight;
+const WINDOW_WIDTH = window.innerWidth;
+const ROAD_COLORS = [0xff0000,0xff8800,0xffff00,0x00ff00,0x00ffff,0x0000ff,0xff00ff];
+const NUMBER_COLORS = ROAD_COLORS.length;
+const WORLD_HEIGHT = 500;
+const WORLD_WIDTH = 500;
